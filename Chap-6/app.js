c7d5a52e76efff51db6b7acf7a24f5b8398e257f
marcus quigley <marcusquigley@gmail.com>
2019-09-25 10:15:16 -0400

    adding UI func. Adding local eslint rules

--- Chap-6/app.js
+++ Chap-6/app.js
@@ -1,5 +1,5 @@
 /* eslint-disable func-names */
-const RETURN_CODE = 13;
+
 
 // budget controller
 // eslint-disable-next-line func-names
@@ -9,28 +9,45 @@ const budgetController = (function () {
 
 // ui controller
 const uiController = (function () {
+	const DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn',
+	};
+
+	return {
+		getInput() {
+			return {
+				type: document.querySelector(DOMstrings.inputType).value,
+				description: document.querySelector(DOMstrings.inputDescription).value,
+				value: document.querySelector(DOMstrings.inputValue).value,
+			};
+		},
+		getDOMStrings() {
+			return DOMstrings;
+		},
+	};
+}());
+
+// global controller
+const controller = (function (budgetCtrl, uiCtrl) {
+	const RETURN_CODE = 13;
+
 	function addItem() {
 		// get input data
-		// const itemType = document.querySelector('.add__type');
-		// const itemDescription = document.querySelector('.add__description');
-		// const itemValue = document.querySelector('.add__value');
-		// console.log(itemDescription);
-		// console.log(itemValue);
+		const input = uiCtrl.getInput();
+		console.log(input);
 		// add item to budget controller
 		// add item to UI
 		// calc budget
 		// display button
 	}
-	document.querySelector('.add__btn').addEventListener('click', addItem);
-	document.addEventListener('keypress', function (event) {
-		event.preventDefault();
+	document.querySelector(uiCtrl.getDOMStrings().inputBtn).addEventListener('click', addItem);
+	document.addEventListener('keypress', (event) => {
+		// 	event.preventDefault();
 		if (event.keyCode === RETURN_CODE || event.which === RETURN_CODE) {
 			addItem();
 		}
 	});
-}());
-
-// global controller
-const controller = (function (budgetCtrl, uiCtrl) {
-
-})(budgetController, uiController);
+}(budgetController, uiController));
